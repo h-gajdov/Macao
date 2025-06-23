9fcd770ee0d12a3abd5eab2ea66b2a5a776229fa
AdamDavis08 <hristijangajdov@gmail.com>
2025-06-23 18:06:42 +0200

    Initial options panel

--- Assets/MainMenuManager.cs
+++ Assets/MainMenuManager.cs
@@ -3,7 +3,18 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class MainMenuManager : MonoBehaviour {
+    public GameObject optionsPanel;
+
+    private void Start() {
+        optionsPanel.SetActive(false);
+    }
+
     public void Quit() {
         Application.Quit();
+        Debug.Log("Quiting..");
+    }
+
+    public void Options() {
+        optionsPanel.SetActive(true);
     }
 }
